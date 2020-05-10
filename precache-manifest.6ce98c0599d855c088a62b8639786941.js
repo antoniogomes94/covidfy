69f67803cff9b6b8a207bb9c6c1f08303782356c
Antonio Gomes <antoniogomes313@outlook.com>
2020-05-10 19:04:23 -0300

    Updates

---
+++ precache-manifest.6ce98c0599d855c088a62b8639786941.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0cb5946f372e1aaf967cbc5c01260b",
+    "url": "/covidfy/index.html"
+  },
+  {
+    "revision": "03cc3d1c6d1c1afa7dc1",
+    "url": "/covidfy/static/css/main.19ea4646.chunk.css"
+  },
+  {
+    "revision": "3bf0bfd638dd35901cfe",
+    "url": "/covidfy/static/js/2.bc37162a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covidfy/static/js/2.bc37162a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03cc3d1c6d1c1afa7dc1",
+    "url": "/covidfy/static/js/main.d8de91c7.chunk.js"
+  },
+  {
+    "revision": "ec312e3881bbb7be94b4",
+    "url": "/covidfy/static/js/runtime-main.4e875e76.js"
+  }
+]);
