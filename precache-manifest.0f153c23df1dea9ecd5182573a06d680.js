fa22d907d904c6153517ea981b01c795637f54cb
Antonio Gomes <antoniogomes313@outlook.com>
2020-05-13 18:32:12 -0300

    Updates

--- precache-manifest.0f153c23df1dea9ecd5182573a06d680.js
+++ precache-manifest.0f153c23df1dea9ecd5182573a06d680.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "66a9c32e8a53a85d210a34582185546f",
+    "revision": "b50eabcdae4d710e5fd1f5a0453bb942",
     "url": "/covidfy/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covidfy/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "1bf91592c23db70d256e",
-    "url": "/covidfy/static/css/main.79eca7fd.chunk.css"
+    "revision": "404749f4c5bbb9605233",
+    "url": "/covidfy/static/css/main.e44fa53e.chunk.css"
   },
   {
     "revision": "9747e98b5d2a02f56dbb",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covidfy/static/js/2.2e3b5267.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1bf91592c23db70d256e",
+    "revision": "404749f4c5bbb9605233",
     "url": "/covidfy/static/js/main.655b87e0.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covidfy/precache-manifest.544715f3df47025546c81ce66e54f928.js"
+  "/covidfy/precache-manifest.0f153c23df1dea9ecd5182573a06d680.js"
 );
 
 self.addEventListener('message', (event) => {
