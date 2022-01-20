a802b9885ef9f5d09401002c733cfd62cdd8cd29
Antonio Gomes <antoniogomes313@outlook.com>
2022-01-19 21:04:53 -0300

    Teste GH Pages

---
+++ build/precache-manifest.87f55be2761c975574341447818c798c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d4676f3692b95aad0b5c66252c941a",
+    "url": "/covidfy/index.html"
+  },
+  {
+    "revision": "afdc2e5869f705bc985f",
+    "url": "/covidfy/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1dc6481e86ba715745e7",
+    "url": "/covidfy/static/css/main.9fc76fa1.chunk.css"
+  },
+  {
+    "revision": "afdc2e5869f705bc985f",
+    "url": "/covidfy/static/js/2.bb33be8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covidfy/static/js/2.bb33be8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dc6481e86ba715745e7",
+    "url": "/covidfy/static/js/main.e1c627a7.chunk.js"
+  },
+  {
+    "revision": "ec312e3881bbb7be94b4",
+    "url": "/covidfy/static/js/runtime-main.4e875e76.js"
+  },
+  {
+    "revision": "8b7ceb75d90a17eab02813a4461b9c32",
+    "url": "/covidfy/static/media/foto.8b7ceb75.jpeg"
+  },
+  {
+    "revision": "3e93eb0e451a534001ab04e5b27917a8",
+    "url": "/covidfy/static/media/logo.3e93eb0e.png"
+  }
+]);
